ef78e7b9001e30adc761a38c763d2ac49c1c4441
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-26 11:34:05 +0800

    add category producs

--- src/Http/Resources/Api/V1/Shop/Catalog/CategoryResource.php
+++ src/Http/Resources/Api/V1/Shop/Catalog/CategoryResource.php
@@ -6,4 +6,32 @@ use NexaMerchant\Apis\Http\Resources\Api\V1\Admin\Catalog\CategoryResource as Ad
 
 class CategoryResource extends AdminCategoryResource
 {
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /* assign category */
+        $category = $this->category ? $this->category : $this;
+
+        /* generating resource */
+        return [
+            /* category's information */
+            'id'                 => $category->id,
+            'name'               => $category->name,
+            'slug'               => $category->slug,
+            'description'        => $category->description,
+            'image'              => $category->image,
+            'status'             => $category->status,
+            'products_count'     => $category->products_count,
+            'parent_id'          => $category->parent_id,
+            'products'           => ProductResource::collection($category->products),
+            'created_at'         => $category->created_at,
+            'updated_at'         => $category->updated_at,
+        ];
+    }
 }
