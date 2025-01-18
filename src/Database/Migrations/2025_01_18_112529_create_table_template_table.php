0528de29864d7e248097194d6a4ccf2cb829b13b
xxl4 <nice.lizhi@gmail.com>
2025-01-18 03:35:45 +0000

    数据库文件

---
+++ src/Database/Migrations/2025_01_18_112529_create_table_template_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('template', function (Blueprint $table) {
+            $table->id();
+            $table->string('template_name');
+            $table->string('template_banner');
+            $table->string('des');
+            $table->string('template_link');
+            $table->string('template_countent');
+            $table->string('created_at');
+            $table->string('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('template');
+    }
+};
