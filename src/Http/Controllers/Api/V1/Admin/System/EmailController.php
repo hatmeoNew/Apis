db2b2f1225eb9bc921af0ee008bb54a4f3442686
xxl4 <nice.lizhi@gmail.com>
2025-02-21 06:27:34 +0000

    add manual email interface

---
+++ src/Http/Controllers/Api/V1/Admin/System/EmailController.php
@@ -0,0 +1,15 @@
+<?php
+namespace NexaMerchant\Apis\Http\Controllers\Api\V1\Admin\System;
+
+use NexaMerchant\Apis\Http\Controllers\Api\V1\Admin\AdminController;
+
+
+class EmailController extends AdminController
+{
+    
+    // manual send order email
+    public function sendOrderEmail()
+    {
+        
+    }
+}
