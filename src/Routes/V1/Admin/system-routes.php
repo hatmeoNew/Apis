9db3fa9c5dd30b2b8268c7823d04fa4acaeb71f6
xxl4 <nice.lizhi@gmail.com>
2025-03-05 03:41:28 +0000

    add faq list

--- src/Routes/V1/Admin/system-routes.php
+++ src/Routes/V1/Admin/system-routes.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use NexaMerchant\Apis\Http\Controllers\Api\V1\Admin\System\TinyMCEController;
 use NexaMerchant\Apis\Http\Controllers\Api\V1\Admin\System\EmailController;
+use NexaMerchant\Apis\Http\Controllers\Api\V1\Admin\System\FaqController;
 
 /**
  * System routes.
@@ -22,5 +23,11 @@ Route::group([
     // email routes
     Route::controller(EmailController::class)->prefix('email')->group(function () {
         Route::post('send-order-email/{order_id}/{email_type}', 'sendOrderEmail');
+    });
+
+    // faq routes
+    Route::controller(FaqController::class)->prefix('faq')->group(function () {
+        Route::get('list', 'index');
+        Route::post('save', 'store');
     });
 });
