0f5db5909d070edc4729d3dbae5e034285f0e1ff
xxl4 <nice.lizhi@gmail.com>
2025-03-25 07:29:41 +0000

    fix options

--- src/Routes/V1/Admin/admin.php
+++ src/Routes/V1/Admin/admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix'     => 'api/v1/admin',
-    'middleware' => ['sanctum.locale','assign_request_id'],
+    'middleware' => ['sanctum.locale','assign_request_id','api.admin.option.log'],
 ], function () {
     /**
      * Authentication routes.
